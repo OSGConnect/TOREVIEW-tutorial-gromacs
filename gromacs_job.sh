6c933b8965053a8dcd6a85fcc2a83df4049a2c97
bala <dmbala@gmail.com>
2015-07-13 12:32:41 -0500

    upload the working example files

--- gromacs_job.sh
+++ gromacs_job.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 module load gromacs/5.0.5
-gmx mdrun -ntmpi 1 -ntomp 1 -nt 1 -deffnm 1cta_nvt.tpr -nsteps 500
+/cvmfs/oasis.opensciencegrid.org/osg/modules/gromacs-5.0.5/bin/gmx mdrun -ntmpi 1 -ntomp 1 -nt 1 -deffnm 1cta_nvt.tpr -nsteps 500
 
